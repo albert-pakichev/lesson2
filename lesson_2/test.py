4e1f2cc70d18019951799ed07dabedc9faee15c2
pakichevalbert <albertpakichev@gmail.ru>
2020-08-12 21:28:05 +0400

    lesson2

---
+++ lesson_2/test.py
@@ -0,0 +1,38 @@
+#original_password='x777'
+#password=input('Введите пароль:')
+#acces=0
+#if password==original_password:
+ #   print('Пароль принят,добро пожаловать в систему')
+  #  acces=1
+#else:
+ #       print('Пароль неверен,вход запрещен')
+# x=input('Say yes or no:')
+# while x=='Yes':
+#     x=input('Say yes or no:')
+# print('Yeeees')
+# a=0
+# while a>=0:
+#  if a==7:
+#    break
+# a+=1
+# print("a")
+# a=-1
+# while a<10:
+#     a+=1
+#     if a==7 or a%2==1:
+#         continue
+# print('a--',a)
+# a=5
+# while a>0:
+#     print("!")
+#     a=a+1
+# word_str = "Hello,world!"
+# for l in word_str:
+#     print(l)
+#     lst=[1,3,5,7,9]
+#     for i in lst:
+#         print(i**2)
+
+# lst = [1,3,5,7,9]
+# for i in lst:
+#     print(i**2)
